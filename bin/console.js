2e4b62eb668053dcd77ff32f2d8f8a1d9752a287
Tom Heinan <tom.heinan@clio.com>
2023-06-08 18:44:11 -0700

    Convert from Slack to Discord

--- bin/console.js
+++ bin/console.js
@@ -1,8 +1,6 @@
 import * as repl from 'repl'
 import * as vm from 'vm'
 
-//import { summarize } from '../lib/gcloud.js'
-
 repl.start().context = vm.createContext({
-  //summarize: summarize,
+  //registerCommands: registerCommands,
 })
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-import pino from "pino";
-
-import { bot } from "./lib/bot.js";
-import * as pkg from './package.json' assert { type: 'json' };
-
-const logger = pino();
+import { bot } from './lib/bot.js';
+import { pool } from './lib/db.js';
+import { logger } from './lib/logger.js';
 
 (async () => {
-  await bot.start();
-  logger.info(`agbot v${pkg.default.version} is up and running!`);
+  logger.info(`Checking database connection...`);
+  const connection = await pool.connect();
+  try {
+    const now = await connection.query('SELECT NOW()');
+    logger.info(`Database connection established at ${now.rows[0].now}`);
+  } finally {
+    connection.release();
+  }
+
+  logger.info(`Initializing...`);
+  await bot.initialize();
+
+  logger.info(`Connecting...`);
+  await bot.login(process.env.DISCORD_TOKEN);
 })();
